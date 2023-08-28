30cc5c6940e8ef18c57ceac068547d20ebe40986
RITIK CHAUDHARY <ritikchaudhary408@gmail.com>
2023-08-28 09:10:37 +0530

    FindAverageMarks
    Write a program to input marks of three tests of a student . Then calculate and print the average of all test marks.

---
+++ FindAverageMarks.py
@@ -0,0 +1,9 @@
+# Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+
+student_1 = int(input())
+student_2 = int(input())
+student_3 = int(input())
+
+Avg = (student_1 + student_2 + student_3) / 3
+
+print(Avg)
