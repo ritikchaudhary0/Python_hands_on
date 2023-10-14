16d2a5f228ca3311189478406bfd0bcbe5be6b26
RITIK CHAUDHARY <ritikchaudhary408@gmail.com>
2023-10-14 22:04:23 +0530

    Update and rename Find X raised to power N to Find X raised to power N.py

---
+++ Find X raised to power N.py
@@ -0,0 +1,10 @@
+# You are given two numbers ’x’(it’s a float), and ’n’(it’s a integer).
+#Your task is to calculate ‘x’ raised to power ‘n’, and return it.
+#The expected time complexity is ’O(logn)’, and the expected space complexity is ’O(1)’, where ‘n’ is the power to which the number should be raised.
+
+X = int(input())
+N = int(input())
+
+Y = X**N
+
+print()
